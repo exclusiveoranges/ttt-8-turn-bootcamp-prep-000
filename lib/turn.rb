5b2d9a69876275792d28075b17315d64d71a51c8
Ryan O'Toole <otoolerj@gmail.com>
2018-03-14 16:31:18 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ end
 
 
 def valid_move?(board, index)
-  if index.between?(0, 8) && position_taken?(board, index) == false
+  if index.between?(0, 8) && !position_taken?(board, index)
     return true
   end
 end
@@ -42,7 +42,7 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-    if valid_move?(board, index) == false
+    if valid_move?(board, index) ==
       move(board, index)
       display_board(board)
     else turn(board)
